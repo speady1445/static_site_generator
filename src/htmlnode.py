68d6ce975e40cb1fd268b56981c97cc39e02340e
speady1445 <sykora1445@gmail.com>
2024-03-11 12:50:15 +0100

    Add class to handle childless htmlnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,3 +23,23 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: str | None,
+        value: str,
+        props: dict[str, str] | None = None,
+    ) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- tests/test_htmlnode.py
+++ tests/test_htmlnode.py
@@ -1,9 +1,9 @@
 import unittest
 
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, LeafNode
 
 
-class TextHTMLNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_to_html(self):
         node = HTMLNode()
         self.assertRaises(NotImplementedError, node.to_html)
@@ -21,4 +21,20 @@ class TextHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             repr(node), "HTMLNode(div, This is a text node, [], {'prop1': 'value1'})"
+        )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "This is a text node")
+        self.assertEqual(node.to_html(), "This is a text node")
+
+    def test_to_html(self):
+        node = LeafNode("div", "This is a text node")
+        self.assertEqual(node.to_html(), "<div>This is a text node</div>")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
         )
