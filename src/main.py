4aba174618c9f784326559dd592ad9d7aaa97f3b
speady1445 <sykora1445@gmail.com>
2024-03-13 09:39:34 +0100

    Feat generate page (markdown -> html)

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import shutil
 from os import rmdir
 from pathlib import Path
 
+from markdown_processing import extract_title, markdown_to_html_node
+
 PUBLIC_FOLDER = Path("public")
 STATIC_FOLDER = Path("static")
 
@@ -41,6 +43,24 @@ def copy_folder(source: Path, destination: Path) -> None:
             sub_destination.mkdir()
             print(f"Copying folder: '{file}' to '{sub_destination}'")
             copy_folder(file, sub_destination)
+
+
+def generate_page(from_path: Path, template_path: Path, dest_path: Path):
+    print(
+        f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'"
+    )
+    markdown_content = from_path.read_text()
+    template = template_path.read_text()
+    html = markdown_to_html_node(markdown_content).to_html()
+    title = extract_title(markdown_content)
+
+    dest_folder = dest_path.parent
+    if not dest_folder.exists():
+        print(f"Creating folder: '{dest_folder}'")
+        dest_folder.mkdir()
+    dest_path.write_text(
+        template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    )
 
 
 if __name__ == "__main__":
